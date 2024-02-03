c0c46cbe589ff37f0bd074cdc5f872cd69ddf466
Ochiengsteven <stevenochieng6305@gmail.com>
2024-02-03 19:09:03 +0300

    ADD NAME COLUMNS TO USERS

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_03_152633) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_160607) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_03_152633) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
