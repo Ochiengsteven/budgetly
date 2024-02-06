ec2effeb19ec076a590620b574ed3e161a2bfe32
Ochiengsteven <stevenochieng6305@gmail.com>
2024-02-06 16:39:07 +0300

    ADD NEW GROUP CONTROLER AND VIEW

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,26 +1,26 @@
 #  : true
 
 class Group < ApplicationRecord
-    belongs_to :user
-    # has_many :entities, dependent: :destroy
-  
-    validates :name, presence: true
-    validates :icon, presence: true
-  
-    validate :valid_icon_format
-  
-    private
-  
-    def valid_icon_format
-      return if icon.blank?
-  
-      unless icon.is_a?(String)
-        errors.add(:icon, 'must be a string')
-        return
-      end
-  
-      return if icon.start_with?('https://') && (icon.end_with?('.png') || icon.end_with?('.jpg'))
-  
-      errors.add(:icon, 'It must have a valid format (https:// .png or .jpg)')
+  belongs_to :user
+  # has_many :entities, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
+  # validate :valid_icon_format
+
+  private
+
+  def valid_icon_format
+    return if icon.blank?
+
+    unless icon.is_a?(String)
+      errors.add(:icon, 'must be a string')
+      return
     end
+
+    return if icon.start_with?('https://') && (icon.end_with?('.png') || icon.end_with?('.jpg'))
+
+    errors.add(:icon, 'It must have a valid format (https:// .png or .jpg)')
   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources  :users, only: %i[index]
-  resources :groups, only: %i[index new create destroy]
+  resources :groups, only: %i[index new create destroy] do
+    resources :entities, only: %i[index show new create destroy]
+  end
 
   root to: 'users#index'
 end
