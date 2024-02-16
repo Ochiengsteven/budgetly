2f54da136dff625e5e30142ddc74ec70fe6be7dc
Ochiengsteven <stevenochieng6305@gmail.com>
2024-02-16 13:53:21 +0300

    UPDATE THE CONFIG FILES

--- config/puma.rb
+++ config/puma.rb
@@ -20,7 +20,6 @@ end
 
 workers ENV.fetch('WEB_CONCURRENCY') { 4 }
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
