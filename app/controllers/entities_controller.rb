40fe45fb6b4aa179f09af8425243a65fe0855117
Ochiengsteven <stevenochieng6305@gmail.com>
2024-02-16 13:40:14 +0300

    MODIFY THE ENTITY INDEX CONTROLLER

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -4,7 +4,6 @@ class EntitiesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     @entities = @group.entities.order(created_at: :desc)
-    @total = @entities.sum(:amount)
     @user = current_user
   end
 
